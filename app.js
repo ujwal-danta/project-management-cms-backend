de88524e3ba61b1c8a181b9ac0ee20926ac56394
ujwal-danta <ujwaldanta@gmail.com>
2023-02-10 08:41:03 +0530

    set up mongoose

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
-const port = 4000
+require('dotenv').config()
+const port = process.env.PORT
+mongoose.set('strictQuery', false);
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 4000, () => {
     console.log(`App running on port ${port}`)
 })
+
+const start = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log('db connected')
+    }
+    catch (err) {
+        console.log('Unable to connect to DB due to - ', err)
+    }
+}
+
+start()
