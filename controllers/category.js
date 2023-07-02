51fc4f7012ef4be9ea4eb663d3f0e6a5d460365a
ujwal-danta <ujwaldanta@gmail.com>
2023-07-02 23:12:17 +0530

    sorted categories

--- controllers/category.js
+++ controllers/category.js
@@ -5,7 +5,7 @@ const Category = require('../models/category')
 
 const getAllCategories = async (req,res,next)=>{
     try {
-        const data = await Category.find()
+        const data = await Category.find().sort({title: 1})
         res.status(200).json(data)
     } catch (error) {
         res.status(500).json(error)
